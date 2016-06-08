0241daf00b2e827e7fe2fa812f832d3d0eabb07c
Jom3es12 <jtas1999@gmail.com>
2016-06-08 00:32:11 -0500

    Cleaned up
    Cleaned up and fixed error on the linux side of things. I really want to
    make the output easier to read. Added a nifty feature on the linux side
    where typing exit will close the program. Previously it would close when
    you pressed enter.

--- swipe me.bat
+++ swipe me.bat
@@ -1,6 +1,6 @@
 @echo off 
 color 0a 
-
+ echo Begin program, ctrl+c to exit.
 :start
 
 echo ========== 
@@ -13,5 +13,16 @@ echo ========== ========== >>swipeLog.txt
 
 goto start
 
-;09441?+4513852?
-%TES??
+
+::Format:
+:: First_Last_Grade/teacher
+:: ex. 
+::  John_Monroe_12
+::  Jane_Jackson_Teacher
+:: Will of cours ehave symbols at beggining and end of the outputted data. 
+:: Copyright (c) 2016 M3DevCorp for Solid Rock UPC
+:: Licensed under the MIT License(MIT)
+::Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, :: and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+::
+::The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+::THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- swipe.sh
+++ swipe.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
-echo "Welcome!"
+echo "Begin Program, type exit to quit program"
 while [ 1 ]
 do 
 
         echo "Please swipe card"
         read data
         
-        if [ "$data" = ""]
-        echo "Goodbye!" 
+        if [ "$data" = "exit" ]
+       then
+        echo "Shutting down!" 
+        sleep 2
+        echo "End Program, Thank you for using."
         exit 0
     fi 
     
-    clear 
+     
     name=$(echo "$data")
+    
+    
+echo "===============">> swipeLog.txt
+echo "Name: $name">> swipeLog.txt
+echo "Terminal: 1">> swipeLog.txt
+data>> swipeLog.txt
 
-echo "Name: $name"
 
 done
-%;JAMESSTURTEVANT^STUDENT/12?+4513852?
-%;JAMESSTURTEVANT^STUDENT/12?;09441?+4513852?
-%;JAMESSTURTEVANT^STUDENT/12?;09441?+4513852?
+
 
 #Format:
-# ;JohnDoe^student^grade 
+# First_Last_Grade/teacher
+# ex. 
+#  John_Monroe_12
+#  Jane_Jackson_Teacher
+# Will of cours ehave symbols at beggining and end of the outputted data. 
+# Copyright (c) 2016 M3DevCorp for Solid Rock UPC
+# Licensed under the MIT License(MIT)
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, # and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
+
+
+
