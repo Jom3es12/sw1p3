b2c6324baedf981407d0e4f31b4960d74dcfe876
Jom3es12 <jtas1999@gmail.com>
2016-06-07 23:19:16 -0500

    Added files
    The basic files with bits of code in them. The .sh file is on my VPS
    somewhere... I need to find it lmao.

---
+++ swipe me.bat
@@ -0,0 +1,17 @@
+@echo off 
+color 0a 
+
+:start
+
+echo ========== 
+echo Swipe card
+echo ==========
+
+ SET /p SN=Card- 
+echo ========== ========== >>swipeLog.txt
+ echo %SN% %time% %date%  >>swipeLog.txt
+
+goto start
+
+;09441?+4513852?
+%TES??
---
+++ swipe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Welcome!"
+while [ 1 ]
+do 
+
+        echo "Please swipe card"
+        read data
+        
+        if [ "$data" = ""]
+        echo "Goodbye!" 
+        exit 0
+    fi 
+    
+    clear 
+    name=$(echo "$data")
+
+echo "Name: $name"
+
+done
+%;JAMESSTURTEVANT^STUDENT/12?+4513852?
+%;JAMESSTURTEVANT^STUDENT/12?;09441?+4513852?
+%;JAMESSTURTEVANT^STUDENT/12?;09441?+4513852?
+
+#Format:
+# ;JohnDoe^student^grade 
